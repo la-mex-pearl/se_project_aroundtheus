ee257419de63f513a262e4a046d28752ccdc8b8a
la-mex-pearl <la.mex.pearl@gmail.com>
2025-07-30 21:04:49 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initalCards = [
+let initialCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,4 +25,4 @@ let initalCards = [
   },
 ];
 
-console.log(initalCards);
+console.log(initialCards);
