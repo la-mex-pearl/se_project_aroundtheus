7525bb9748ba386b56ec7547972bc6046de6dc24
la-mex-pearl <la.mex.pearl@gmail.com>
2025-08-18 20:04:19 -0600

    modal title update

--- scripts/index.js
+++ scripts/index.js
@@ -51,6 +51,8 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
   cardTitleEl.textContent = cardData.name;
   return cardElement;
 }
